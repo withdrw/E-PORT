6756fefec1666414dee1fd77b7998b8baabe61ed
withdrw <finesse@usa.com>
2023-04-04 18:15:08 -0500

    footer done , work on contact modal

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import Header from './sections/Header';
 import Home from './sections/Home';
 import Projects from './sections/Projects';
 import Footer from './sections/Footer';
+import { useState } from 'react';
 
 function App() {
+
+  const [setModal, showModal] = useState('false')
+  
+
+
+
+
+
   return (
     <>
       <Header></Header>
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className= ' flex text-[white] h-[100px] items-center justify-center '>
-        <footer className='text-[16px]'>
+    <div className= ' flex text-[white]  items-center justify-center text-center w-[100%] bg-black mt-10 '>
+        <footer className='text-[16px] w-[100%]'>
 
+    <div className='text-[32px] mt-16'>
 
             This website was created by Faiz using React JS.
+    </div>
 
-            <a className='ml-8 font-semibold cursor-pointer ' href="https://github.com/withdrw">Github</a>
-            <a className='ml-8 font-semibold cursor-pointer ' href="/">Contact</a>
-            <a className='ml-8 font-semibold cursor-pointer ' href="/">About</a>
+<div className='mt-12 mb-10'>
 
-        <div className='border-b-3 border mt-2 '>
-        </div>
-        <div className=''>
-            copyright 2023
-        </div>
+            <a className='mx-10 hover:underline transition-all mb-10 font-semibold cursor-pointer ' href="https://github.com/withdrw">Github</a>
+            <button className='mx-10 hover:underline transition-all mb-10 font-semibold cursor-pointer ' href="/">Contact</button>
+            <a className='mx-10 hover:underline transition-all mb-10 font-semibold cursor-pointer ' href="/">About</a>
+</div>
+
         
         </footer>
     </div>
