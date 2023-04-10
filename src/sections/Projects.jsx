16cc27283c6fe52b716ee1d0022bd391e7c74824
withdrw <finesse@usa.com>
2023-04-10 14:52:26 -0500

    added animations on the projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 function Projects() {
   return (
@@ -6,9 +10,17 @@ function Projects() {
         <div className='flex-col h-[100%] mt-20 text-[30px] font-semibold text-center text-[white] '>
         HERE ARE MY PROJECTS 
           <div  className=' mt-16'>
+            <div  data-aos="fade-down-right">
     <img  className='w-[100%]' src="https://media.discordapp.net/attachments/942157875323748363/1092553981512929311/Screenshot_2023-03-29_160931.png?width=960&height=483" alt="" />
+            </div>
+<div  data-aos="fade-down-left">
+
           <img className='w-[100%] mt-20' src="https://media.discordapp.net/attachments/942157875323748363/1092553981756182528/Screenshot_2023-03-29_160447.png?width=1333&height=671" alt="" />
+</div>
+<div  data-aos="fade-down-right">
+
           <img className='w-[100%] mt-20' src="https://media.discordapp.net/attachments/942157875323748363/1092553982209175582/Screenshot_2023-03-29_160803.png?width=1346&height=671" alt="" />
+</div>
           </div>
         </div>
     </div>
