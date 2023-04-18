e9ae9104056c2aa1406821b6aff0e7836549b4b7
withdrw <finesse@usa.com>
2023-04-18 16:57:25 -0500

    style projects and descriptions

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -12,34 +12,41 @@ function Projects() {
         <div className='flex-col h-[100%] mt-20 text-[30px] font-semibold text-center text-[white] '>
         HERE ARE MY PROJECTS 
           <div  className=' mt-16'>
-            <div className='data__para '  data-aos="fade-down-right">
-              
+            <div className='flex flex-col data__para items-center '  data-aos="fade-down-right">
 
-              
-              <p  className='img__para absolute '> 
+              <div className='w-[80%] relative'> 
+
+                <div className='img__para bg-[gray]/30 absolute w-[100%] h-[100%]'>
+              <p> 
                 This project was created for users to chat with others about things in common and post threads to interact with others.
               </p>
-    <img className='img__one img__two w-[80%] ml-40' src="https://media.discordapp.net/attachments/942157875323748363/1092553981512929311/Screenshot_2023-03-29_160931.png?width=960&height=483" alt="" />
-      <h1>
-        Forum 
-      </h1>
+                </div>
+    <img className=' img__two w-[80%]' src="https://media.discordapp.net/attachments/942157875323748363/1092553981512929311/Screenshot_2023-03-29_160931.png?width=960&height=483" alt="" />
+              </div>
       
             </div>
-<div classname='data__para' data-aos="fade-down-left">
-  <p className='img__para '>
+<div classname='flex flex-col data__para items-center' data-aos="fade-down-left">
+  <div className='w-[80%] relative'>
+  <div className='img__para bg-[gray]/30 absolute w-[100%] h-[100%]'>
+  <p>
     Netflix clone made using Typescript. Receives backend data from TMDBApi and YoutubeApi to get movie & show trailers. Also has real ratings for each movie and show on the site.
   </p>
+    </div>    
 
-          <img className='w-[80%] img__two mt-20' src="https://media.discordapp.net/attachments/942157875323748363/1092553981756182528/Screenshot_2023-03-29_160447.png?width=1333&height=671" alt="" />
-    Netflix Clone
+          <img className='img__two w-[80%]' src="https://media.discordapp.net/attachments/942157875323748363/1092553981756182528/Screenshot_2023-03-29_160447.png?width=1333&height=671" alt="" />
+  </div>
 </div>
-<div  data-aos="fade-down-right">
-  <p className='absolute'>
+<div className='flex flex-col data__para items-center'  data-aos="fade-down-right">
+  <div className='w-[80%] relative'>
+<div className='img__para bg-[gray]/30 absolute w-[100%] h-[100%]'>
+  <p>
     A library created using JavaScript,HTML,CSS one of the first projects I first worked on.
   </p>
+</div>
+  
 
-          <img className='img__one  mt-20' src="https://media.discordapp.net/attachments/942157875323748363/1092553982209175582/Screenshot_2023-03-29_160803.png?width=1346&height=671" alt="" />
-          Library
+          <img className='img__two w-[100%]  mt-20' src="https://media.discordapp.net/attachments/942157875323748363/1092553982209175582/Screenshot_2023-03-29_160803.png?width=1346&height=671" alt="" />
+  </div>
 </div>
           </div>
         </div>
